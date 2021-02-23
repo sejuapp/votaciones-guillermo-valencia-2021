ba6bed796980b296d2528c10eca44333189225fe
DESKTOP-3SRVM78\ASUS-S510UN <givandavid@gmail.com>
2021-02-23 00:25:05 -0500

    actualizamos conexiones para base de datos local

--- conexionBD.php
+++ conexionBD.php
@@ -4,13 +4,13 @@ function conectarse()
 
 {
 
-$db_host="bxgpkrzi7babzgz8skdo-mysql.services.clever-cloud.com"; // Host BD al que conectarse, habitualmente es localhost
+$db_host="localhost"; // Host BD al que conectarse, habitualmente es localhost
 
-$db_nombre="bxgpkrzi7babzgz8skdo"; // Nombre de la Base de Datos que se desea utilizar
+$db_nombre="votaciones-2021"; // Nombre de la Base de Datos que se desea utilizar
 
-$db_user="uoyb6yzzi1qaizoy"; // Nombre del usuario con permisos para acceder a la BD
+$db_user="root"; // Nombre del usuario con permisos para acceder a la BD
 
-$db_pass="MOLwhh8HdJP7so9p9o6t"; // Contraseña del usuario de la BD
+$db_pass="votaciones2021"; // Contraseï¿½a del usuario de la BD
 
 
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_nombre);
